019e07a828f54ffe7c698e252cad1997ec4ef7d7
suji-anandkumar <134618343+suji-anandkumar@users.noreply.github.com>
2023-08-14 20:54:24 +0100

    Update db.js

--- tests/stepDefinition/db.js
+++ tests/stepDefinition/db.js
@@ -14,3 +14,28 @@ while (stmt.fetchSync()) {
 }
 connection.closeSync();
 
+const connection = mysql.createConnection({
+    host: "your_host",
+    user: "your_username",
+    password: "your_password",
+    database: "your_database"
+});
+connection.connect((error) => {
+    if (error) {
+        console.error("Error connecting:", error);
+        return;
+    }
+    console.log("Connected to the database!");
+    // Perform database operations here
+    connection.end(); // Close the connection when done
+});
+const sqlQuery = "SELECT * FROM your_table";
+connection.query(sqlQuery, (queryError, result) => {
+    if (queryError) {
+        console.error("Query error:", queryError);
+        return;
+    }
+
+    // Process the query results
+    console.log("Query results:", result);
+});
