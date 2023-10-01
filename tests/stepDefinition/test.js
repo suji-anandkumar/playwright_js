f546966d78a09716249a40c0b60a8f26409928ca
suji-anandkumar <134618343+suji-anandkumar@users.noreply.github.com>
2023-10-01 20:17:24 +0100

    Update test.js

--- tests/stepDefinition/test.js
+++ tests/stepDefinition/test.js
@@ -7,15 +7,15 @@ function readXmlFile(filePath: string): string {
 }
 
 // Define a function to extract text content using an XPath-like expression
-async function extractTextContent(xmlData: string, relativeXPath: string): Promise<string> {
+async function extractTextContent(xmlData: string, relativeXPath: string): Promise<string | null> {
     try {
         // Parse the XML data using xml2js
         const parsedData = await xml2js.parseStringPromise(xmlData);
 
         // Use the relativeXPath to navigate and extract the text content
-        const result = getObjectByXPath(parsedData, relativeXPath);
+        const result = traverseXml(parsedData, relativeXPath);
 
-        if (result) {
+        if (result !== null) {
             return result;
         } else {
             return 'Not found';
@@ -25,34 +25,47 @@ async function extractTextContent(xmlData: string, relativeXPath: string): Promi
     }
 }
 
-// Helper function to navigate the XML structure using XPath-like syntax
-function getObjectByXPath(obj: any, xpath: string): any {
-    const parts = xpath.split('/');
-    let current = obj;
+// Define a function to recursively traverse the parsed XML object
+function traverseXml(parsedData: any, relativePath: string): string | null {
+    const parts = relativePath.split('/');
+    let current = parsedData;
 
     for (const part of parts) {
-        if (!current || !current.hasOwnProperty(part)) {
+        if (part && current[part]) {
+            current = current[part];
+        } else {
             return null;
         }
-        current = current[part];
     }
 
-    return current;
+    if (typeof current === 'string') {
+        return current;
+    } else {
+        return null;
+    }
 }
 
 // Usage example:
 const xmlFilePath = 'path/to/xml/file.xml';
-const relativeXPath = 'test/test1/name/id'; // Replace with your desired relative XPath
+const relativeXPath = '//tradeDate'; // Use your desired relative XPath
 
 try {
     // Read XML data from the file
     const xmlData = readXmlFile(xmlFilePath);
 
     // Extract text content using the relativeXPath
-    const textContent = await extractTextContent(xmlData, relativeXPath);
-
-    // Now you have the text content as a string
-    console.log('Text Content:', textContent);
+    extractTextContent(xmlData, relativeXPath)
+        .then((textContent) => {
+            if (textContent !== null) {
+                // Now you have the text content as a string
+                console.log('Text Content:', textContent);
+            } else {
+                console.log('Text Content not found');
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 } catch (error) {
     console.error(error);
 }
